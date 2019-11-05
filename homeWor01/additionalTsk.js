775e623bb9d9f478eff891e7fcf1f1c27cb94aae
VladimirSokurenko <dp240191svv1@gmail.com>
2019-11-05 22:23:46 +0200

    first commit

---
+++ homeWor01/additionalTsk.js
@@ -0,0 +1,7 @@
+var numbers = [ 254, 115, 78, 25, 91, 45, 37 ];
+
+for(var iteratior = 0; iteratior < numbers.length; iteratior++){
+    if(numbers[iteratior] > 50){
+        console.log(numbers[iteratior])
+    }
+}
